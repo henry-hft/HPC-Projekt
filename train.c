10f4bf28d2f8857e9df7df947d34ab93a04ec891
henry-hft <333333henry@gmail.com>
2022-01-06 09:43:25 +0100

    Add sigmoid function

--- train.c
+++ train.c
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <algorithm>
 
+using namespace std;
+
+// Sigmoidfunktion
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
