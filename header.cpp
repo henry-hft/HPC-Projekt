afc6654ac14cb032354b47c3b11f529a6d4794a6
henry-hft <333333henry@gmail.com>
2022-01-08 20:56:44 +0100

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,25 @@
+// MNIST Datensatz
+
+
+// Trainingsdaten
+// Labels
+extern const string train_labels = "mnist/train_labels.dat";
+// Bilder
+extern const string train_images = "mnist/train_labels.dat";
+
+// Anzahl der Traningsbilder
+const int train_amount = 50000;
+
+
+// Testdaten
+// Labels
+extern const string test_labels = "mnist/test_labels.dat";
+// Bilder
+extern const string test_images = "mnist/train_images.dat";
+
+// Anzahl der Testbilder
+extern const int test_amount = 10000;
+
+// Größe der Bilder in Pixel
+extern const int width = 28;
+extern const int height = 28;
