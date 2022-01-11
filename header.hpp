c0f98252b287ed59ea6e339af410b686ad2f74ee
henry-hft <333333henry@gmail.com>
2022-01-11 20:08:40 +0100

    Update info() function

--- header.hpp
+++ header.hpp
@@ -1,64 +1,53 @@
-// MNIST Datensatz
-const string folder = "mnist/";
+#include <omp.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <algorithm>
+#include <ctime>
 
-// Trainingsdaten
-// Labels
-extern const string train_labels = folder + "train_labels.dat";
-// Bilder
-extern const string train_images = folder + "train_images.dat";
+using namespace std;
 
-// Anzahl der Traningsbilder
-const int train_amount = 60000;
+#define THREADS 4
+// Training image file name
 
+extern const string train_images = "mnist/train-images.idx3-ubyte";
 
-// Testdaten
-// Labels
-extern const string test_labels = folder + "test_labels.dat";
-// Bilder
-extern const string test_images = folder + "test_images.dat";
+// Training label file name
+extern const string train_labels = "mnist/train-labels.idx1-ubyte";
 
-// Anzahl der Testbilder
-extern const int test_amount = 10000;
+// Weights file name
+extern const string model = "model/model.txt";
 
-// Größe der Bilder in Pixel
+// Report file name
+extern const string report_fn = "training-report.dat";
+
+// Number of training samples
+extern const int train_amount = 60000;
+
+// Image size in MNIST database
 extern const int width = 28;
 extern const int height = 28;
 
+// n1 = Number of input neurons
+// n2 = Number of hidden neurons ResearchPriority1
+// n3 = Number of output neurons
+// epochs = Number of iterations for back-propagation algorithm
+// learning_rate = Learing rate
+// momentum = Momentum (heuristics to optimize back-propagation algorithm)
+// epsilon = Epsilon, no more iterations if the learning error is smaller than epsilon
 
-// Datei, die die trainierten Gewichte enthält
-extern const string model = "model.txt";
-
-// Ausgabedatei Training
-extern const string train_output = "train_output.csv";
-
-// Ausgabedatei Test
-extern const string test_output = "test_output.txt";
-
-// Input Layer
-// Anzahl der Input-Neuronen
-// Ein Input-Neuronen pro Pixel. Da die Bilder in Grauwerten gespeichert sind, koennen diese Werte zwsichen 0 und 255 annehmen.
-extern const int input = width * height; // 28 * 28 = 784 (784 Neuronen)
-
-// Hidden Layer
-extern const int hidden = 128; // 128 Neuronen
-
-// Output Layer
-extern const int output = 10; // 0-9 (10 moegliche Ausgaenge), 10 Neuronen
-
-// Anzahl der Durchläufe durch das Traningsset
-extern const int epochs = 512;
-
-// Lernrate
+extern const int input_layer = width * height; // = 784, without bias neuron 
+extern const int hidden_layer = 128; 
+extern const int output_layer = 10; // Ten classes: 0 - 9
+extern const int epochs = 10;
 extern const double learning_rate = 0.001;
-
-
-// Sigmoidfunktion
-double sigmoid_function(double x) {
-    double result;
-    result = 1 / (1 + exp(-x));
-    return result;
-}
-
 
 // Info
 void info(bool training) {
@@ -76,12 +65,22 @@ void info(bool training) {
     cout << "HPC-Computing Projekt WiSe 21/22" << endl;
     cout << "Neuronales Netz mit OpenMP für MNIST Datensatz" << endl;
     cout << endl;
-    cout << "Input-Layer: " << input << " Neuronen" << endl;
-    cout << "Hidden-Layer: " << hidden << " Neuronen" << endl;
-    cout << "Output-Layer: " << output << " Neuronen" << endl;
-    cout << "Anzahl Epochen: " << epochs << endl;
-    cout << "Lernrate: " << learning_rate << endl;
+    cout << "Input-Layer: " << input_layer << " Neuronen" << endl;
+    cout << "Hidden-Layer: " << hidden_layer << " Neuronen" << endl;
+    cout << "Output-Layer: " << output_layer << " Neuronen" << endl;
+    if(training == true) {
+        cout << "Anzahl Epochen: " << epochs << endl;
+        cout << "Lernrate: " << learning_rate << endl;
+	cout << "Anzahl Threads: " << THREADS << endl;
+    }
     cout << data << " (Bilder): " << train_images << endl;
     cout << data << " (Labels): " << train_labels << endl;
     cout << "Anzahl " << data << ": " << train_amount << endl << endl;
+}
+
+// Sigmoidfunktion
+double sigmoid_function(double x) {
+    double result;
+    result = 1 / (1 + exp(-x));
+    return result;
 }
