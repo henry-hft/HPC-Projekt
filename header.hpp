6f2bfd085cfdd3f758a6e017255a3572354acbde
henry-hft <333333henry@gmail.com>
2022-01-11 20:22:36 +0100

    Add reverse_integer function

--- header.hpp
+++ header.hpp
@@ -84,3 +84,12 @@ double sigmoid_function(double x) {
     result = 1 / (1 + exp(-x));
     return result;
 }
+
+int reverse_integer(int i) {
+    unsigned char ch1, ch2, ch3, ch4;
+    ch1 = i&255;
+    ch2 = (i>>8)&255;
+    ch3 = (i>>16)&255;
+    ch4 = (i>>24)&255;
+    return((int)ch1<<24)+((int)ch2<<16)+((int)ch3<<8)+ch4;
+}
